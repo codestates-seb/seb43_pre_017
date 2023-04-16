2034771c23a6e1230c9fd003d9ebd349293e6487
1-blue <1-blue98@naver.com>
2023-04-16 19:51:34 +0900

    feat: 사이드바 추가 및 메인 레이아웃 수정
    1. 사이드바를 메인 내부로 이동
    2. 사이드바 작성

--- client/src/Layout/SideBar/index.jsx
+++ client/src/Layout/SideBar/index.jsx
@@ -1,11 +1,49 @@
+import { useLocation } from "react-router-dom";
+
+// component
+import Icon from "../../components/common/Icon";
+
 // style
-import StyledSideBar from "./style";
+import StyledSideBar, { StyledLink } from "./style";
 
 /** 2023/04/12 - SideBar 컴포넌트 - by 1-blue */
-const SideBar = () => (
-  <StyledSideBar>
-    <h1>사이드 바</h1>
-  </StyledSideBar>
-);
+const SideBar = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <StyledSideBar>
+      <>
+        <StyledLink to="/" match={pathname === "/" ? "true" : undefined}>
+          <button type="button">
+            <Icon shape="earth" fill={pathname === "/"} />
+          </button>
+          <span>Questions</span>
+        </StyledLink>
+      </>
+      <>
+        <StyledLink
+          to="/users"
+          match={pathname === "/users" ? "true" : undefined}
+        >
+          <button type="button">
+            <Icon shape="users" fill={pathname === "/users"} />
+          </button>
+          <span>Users</span>
+        </StyledLink>
+      </>
+      <>
+        <StyledLink
+          to="/tags"
+          match={pathname === "/tags" ? "true" : undefined}
+        >
+          <button type="button">
+            <Icon shape="tags" fill={pathname === "/tags"} />
+          </button>
+          <span>Tags</span>
+        </StyledLink>
+      </>
+    </StyledSideBar>
+  );
+};
 
 export default SideBar;
---
+++ client/src/pages/Home/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+/** 2023/04/16 - 메인 페이지 스타일 - by 1-blue */
+const StyledMain = styled.article``;
+
+export default StyledMain;
