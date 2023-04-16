64cb0b49aaece76908df9ab53c8e2ed5ee4864fb
전형호 <wjsgudgh12@naver.com>
2023-04-16 13:09:47 +0900

    design: 푸터 로고 생성

--- client/src/Layout/Footer/index.jsx
+++ client/src/Layout/Footer/index.jsx
@@ -1,7 +1,16 @@
 // style
-import StyledFoorter from "./style";
+import StyledFoorter, { StyledLogo } from "./style";
 
 /** 2023/04/12 - 푸터를 감싸는 컴포넌트 - by 1-blue */
-const Footer = () => <StyledFoorter>하단 푸터</StyledFoorter>;
+const Footer = () => (
+  <StyledFoorter>
+    <StyledLogo>
+      <img
+        src="https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon@2.png?v=73d79a89bded"
+        alt="logo"
+      />
+    </StyledLogo>
+  </StyledFoorter>
+);
 
 export default Footer;
--- client/src/Layout/Footer/style.js
+++ client/src/Layout/Footer/style.js
@@ -1,6 +1,21 @@
 import styled from "styled-components";
 
 /** 2023/04/12 - 푸터를 감싸는 컴포넌트의 스타일 - by 1-blue */
-const StyledFoorter = styled.footer``;
+const StyledFoorter = styled.footer`
+  width: 100%;
+  height: 320px;
+  background-color: ${({ theme }) => theme.colors.zinc800};
+`;
+/** 2023/04/16 - 푸터 스택오버플로우 스타일 - by JHH0906 */
+export const StyledLogo = styled.div`
+  width: 11%;
+  height: 320px;
 
+  > img {
+    width: 60px;
+    float: right;
+    margin-top: 6px;
+    margin-right: 8px;
+  }
+`;
 export default StyledFoorter;
