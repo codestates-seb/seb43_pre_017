956f766c7b6fbc472753b40b46fd1928866eddc3
serveman <serveman6325@gmail.com>
2023-04-17 16:02:33 +0900

    fix: AnswerResponseDto

--- server/src/main/java/com/homunculus/preproject/answer/dto/AnswerResponseDto.java
+++ server/src/main/java/com/homunculus/preproject/answer/dto/AnswerResponseDto.java
@@ -1,8 +1,12 @@
 package com.homunculus.preproject.answer.dto;
 
 import com.homunculus.preproject.audit.Auditable;
+import com.homunculus.preproject.response.details.AnswerResponseDetails;
+
+import java.util.List;
 
 public class AnswerResponseDto {
-    private Long answerId;
-    private String content;
+    private String message;
+    private Integer answersCount;    // todo : FE 와 상의 필요
+    private List<AnswerResponseDetails> answers;
 }
