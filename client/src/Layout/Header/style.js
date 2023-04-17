35ec67225d8b3ca3f59ac811f72e825762382391
sinyaenok <dpshrdl56@gmail.com>
2023-04-17 11:46:25 +0900

    헤더 position:sticky 수정

--- client/src/Layout/Header/style.js
+++ client/src/Layout/Header/style.js
@@ -3,11 +3,14 @@ import styled from "styled-components";
 /** 2023/04/16 - 헤더를 감싸는 컴포넌트의 스타일 - by sinyaenok */
 const StyledHeader = styled.header`
   min-height: 50px;
+  min-width: auto;
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-around;
-  position: fixed;
+  position: sticky;
+  top: 0;
+  left: 0;
   background-color: ${({ theme }) => theme.colors.gray50};
   border-top: 3px solid ${({ theme }) => theme.colors.main500};
   box-shadow: 0px 3px 7px ${({ theme }) => theme.colors.gray200};
