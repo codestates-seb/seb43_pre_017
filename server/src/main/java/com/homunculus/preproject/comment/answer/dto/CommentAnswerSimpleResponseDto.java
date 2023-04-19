a54f99f3fe699e6f5ba9877f2175c6558263ff84
serveman <120380939+serveman@users.noreply.github.com>
2023-04-19 13:30:51 +0900

    add: Post, Patch, Delete 응답을 위해 simpleResponseDto 추가 delete: 기존 방식의 Single, Multi 응답 Dto 삭제

---
+++ server/src/main/java/com/homunculus/preproject/comment/answer/dto/CommentAnswerSimpleResponseDto.java
@@ -0,0 +1,12 @@
+package com.homunculus.preproject.comment.answer.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class CommentAnswerSimpleResponseDto {
+    private String message;
+}
---
+++ server/src/main/java/com/homunculus/preproject/comment/article/dto/CommentArticleSimpleResponseDto.java
@@ -0,0 +1,12 @@
+package com.homunculus.preproject.comment.article.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class CommentArticleSimpleResponseDto {
+    private String message;
+}
