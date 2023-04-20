95c1b52ea1544f0314ef2bb34a8c6465e4dde25b
sundae95 <114928363+sundae95@users.noreply.github.com>
2023-04-20 15:01:22 +0900

    feat: EvaluationAnswerController Post 구현

---
+++ server/src/main/java/com/homunculus/preproject/evaluation/answer/controller/EvaluationAnswerController.java
@@ -0,0 +1,40 @@
+package com.homunculus.preproject.evaluation.answer.controller;
+
+import com.homunculus.preproject.evaluation.answer.dto.EvaluationAnswerDto;
+import com.homunculus.preproject.evaluation.answer.dto.EvaluationAnswerResponseDto;
+import com.homunculus.preproject.evaluation.answer.entity.EvaluationAnswer;
+import com.homunculus.preproject.evaluation.answer.mapper.EvaluationAnswerMapper;
+import com.homunculus.preproject.evaluation.answer.service.EvaluationAnswerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+public class EvaluationAnswerController {
+    private static final String EVALUATIONANSWER_DEFAULT_URL = "/api/answer";
+    private static final String EVALUATIONANSWER_DEFAULT_URL_DETAIL = "/evaluation";
+
+    private final EvaluationAnswerService evaluationAnswerService;
+    private final EvaluationAnswerMapper mapper;
+
+    @PostMapping(EVALUATIONANSWER_DEFAULT_URL + "/{answerId}" + EVALUATIONANSWER_DEFAULT_URL_DETAIL)
+    public ResponseEntity postEvaluationAnswer(@Valid @RequestBody EvaluationAnswerDto.Post evaluationAnswerDtoPost,
+                                               @PathVariable("answerId") @Positive Long answerId) {
+        evaluationAnswerDtoPost.setAnswerId(answerId);
+        EvaluationAnswer evaluationAnswer = evaluationAnswerService.createEvaluationAnswer(mapper.evaluationAnswerPostDtoToEvaluationAnswer(evaluationAnswerDtoPost));
+
+        EvaluationAnswerResponseDto responseDto = mapper.evaluationAnswerToEvaluationAnswerResponseDto(evaluationAnswer);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
+
+}
---
+++ server/src/main/java/com/homunculus/preproject/evaluation/answer/service/EvaluationAnswerService.java
@@ -0,0 +1,9 @@
+package com.homunculus.preproject.evaluation.answer.service;
+
+import com.homunculus.preproject.evaluation.answer.entity.EvaluationAnswer;
+
+public class EvaluationAnswerService {
+    public EvaluationAnswer createEvaluationAnswer(EvaluationAnswer evaluation) {
+        return null;
+    }
+}
