a7804a9f3f1655bc78eae787bb6e6e716237e043
serveman <120380939+serveman@users.noreply.github.com>
2023-04-20 21:45:06 +0900

    bug: Evaluation Article, Answer - mapper 에 애너테이션 추가

--- server/src/main/java/com/homunculus/preproject/evaluation/answer/mapper/EvaluationAnswerMapper.java
+++ server/src/main/java/com/homunculus/preproject/evaluation/answer/mapper/EvaluationAnswerMapper.java
@@ -3,7 +3,9 @@ package com.homunculus.preproject.evaluation.answer.mapper;
 import com.homunculus.preproject.evaluation.answer.dto.EvaluationAnswerDto;
 import com.homunculus.preproject.evaluation.answer.dto.EvaluationAnswerResponseDto;
 import com.homunculus.preproject.evaluation.answer.entity.EvaluationAnswer;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface EvaluationAnswerMapper {
     EvaluationAnswer evaluationAnswerPostDtoToEvaluationAnswer(EvaluationAnswerDto.Post evaluationDtoPost);
 
--- server/src/main/java/com/homunculus/preproject/evaluation/article/mapper/EvaluationArticleMapper.java
+++ server/src/main/java/com/homunculus/preproject/evaluation/article/mapper/EvaluationArticleMapper.java
@@ -3,7 +3,9 @@ package com.homunculus.preproject.evaluation.article.mapper;
 import com.homunculus.preproject.evaluation.article.dto.EvaluationArticleDto;
 import com.homunculus.preproject.evaluation.article.dto.EvaluationArticleResponseDto;
 import com.homunculus.preproject.evaluation.article.entity.EvaluationArticle;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface EvaluationArticleMapper {
     EvaluationArticle evaluationArticlePostDtoToEvaluationArticle(EvaluationArticleDto.Post evaluationDtoPost);
 
