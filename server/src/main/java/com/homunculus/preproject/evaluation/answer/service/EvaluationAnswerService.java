ca02f1ee7cc4b68ac0e75f98f2712afc4b3ebab5
serveman <120380939+serveman@users.noreply.github.com>
2023-04-20 21:42:45 +0900

    bug: Evaluation Article, Answer - Service 에 애너테이션 추가

--- server/src/main/java/com/homunculus/preproject/evaluation/answer/service/EvaluationAnswerService.java
+++ server/src/main/java/com/homunculus/preproject/evaluation/answer/service/EvaluationAnswerService.java
@@ -1,7 +1,9 @@
 package com.homunculus.preproject.evaluation.answer.service;
 
 import com.homunculus.preproject.evaluation.answer.entity.EvaluationAnswer;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EvaluationAnswerService {
     public EvaluationAnswer createEvaluationAnswer(EvaluationAnswer evaluation) {
         return null;
