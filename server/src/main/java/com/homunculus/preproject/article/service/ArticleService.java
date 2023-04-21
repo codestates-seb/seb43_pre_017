3ea435e116e82b741f2cb4b7136fbcd40f0a9161
sundae95 <114928363+sundae95@users.noreply.github.com>
2023-04-21 13:02:25 +0900

    feat: ArticleService 의 findVerifiedArticle 메서드 구현

--- server/src/main/java/com/homunculus/preproject/article/service/ArticleService.java
+++ server/src/main/java/com/homunculus/preproject/article/service/ArticleService.java
@@ -1,21 +1,35 @@
 package com.homunculus.preproject.article.service;
 
+import com.homunculus.preproject.answer.entity.Answer;
 import com.homunculus.preproject.article.entity.Article;
+import com.homunculus.preproject.article.repository.ArticleRepository;
+import com.homunculus.preproject.exception.BusinessLogicException;
+import com.homunculus.preproject.exception.ExceptionCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
 
+    private final ArticleRepository articleRepository;
     public Article createArticle(Article article) {
 
-        return null;
+        return articleRepository.save(article);
     }
 
     public Article updateArticle(Article article) {
+        // 업데이트를 하기 위해서 DB에 해당 정보가 있는지 확인(수정이니까 해당정보가 있는지 알아야함)
+        // Article findArticle = findVeryfiedArticle(article.getArticleId());
 
+        // 만일 없다면 예외처리 있으면 수정 내용반영
+
+
+        // 반영한 내용을 저장
         return null;
     }
 
@@ -32,5 +46,13 @@ public class ArticleService {
     public void deleteArticle(Long articleId) {
 
         return;
+    }
+    public Article findVerifiedArticle(long articleId) {
+        Optional<Article> optionalArticle =
+                articleRepository.findById(articleId);
+        Article findArticle =
+                optionalArticle.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.ARTICLE_NOT_FOUND));
+        return findArticle;
     }
 }
--- server/src/main/java/com/homunculus/preproject/exception/ExceptionCode.java
+++ server/src/main/java/com/homunculus/preproject/exception/ExceptionCode.java
@@ -3,7 +3,9 @@ package com.homunculus.preproject.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
+
     USER_NOT_FOUND(404, "User not found"),
+    ARTICLE_NOT_FOUND(404, "Article not found"),
     USER_EXISTS(409, "User exists"),
     NOT_IMPLEMENTATION(501, "Not Implementation"),
     INVALID_USER_STATUS(400, "Invalid user status");
