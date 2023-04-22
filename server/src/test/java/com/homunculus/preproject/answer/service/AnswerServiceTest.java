5334a8e99bd1c5a7ad6bded16d692b6c8c082e7e
serveman <120380939+serveman@users.noreply.github.com>
2023-04-23 01:02:02 +0900

    feat: Patch Test Case 실패 케이스 추가 ( 질문글, 답변글, 인증정보 없음 )

--- server/src/test/java/com/homunculus/preproject/answer/service/AnswerServiceTest.java
+++ server/src/test/java/com/homunculus/preproject/answer/service/AnswerServiceTest.java
@@ -64,36 +64,11 @@ class AnswerServiceTest {
         final Long answerId = 1L;
         final String email = "email@gmail.com";
 
-        Answer answer = new Answer();
-        {
-            final String content = "답변의 내용";
-
-            answer.setAnswerId(answerId);
-            answer.setContent(content);
-
-            Member member = new Member();
-            member.setEmail(email);
-            answer.setMember(member);
-
-            Article article = new Article();
-            article.setArticleId(articleId);
-            answer.setArticle(article);
-        }
+        final String content = "답변의 내용";
+        Answer answer = createDummyAnswer(articleId, answerId, content, email);
 
         final String updateContent = "변경된 답변의 내용";
-        Answer updateAnswer = new Answer();
-        {
-            updateAnswer.setAnswerId(answerId);
-            updateAnswer.setContent(updateContent);
-
-            Member member = new Member();
-            member.setEmail(email);
-            updateAnswer.setMember(member);
-
-            Article article = new Article();
-            article.setArticleId(articleId);
-            updateAnswer.setArticle(article);
-        }
+        Answer updateAnswer = createDummyAnswer(articleId, answerId, updateContent, email);
 
         given(answerRepository.save(answer)).willReturn(new Answer());
         given(answerRepository.findById(answer.getAnswerId())).willReturn(Optional.of(answer));
@@ -116,36 +91,11 @@ class AnswerServiceTest {
         final Long answerId = 1L;
         final String email = "notEmail@gmail.com";
 
-        Answer answer = new Answer();
-        {
-            final String content = "답변의 내용";
-
-            answer.setAnswerId(answerId);
-            answer.setContent(content);
-
-            Member member = new Member();
-            member.setEmail(email);
-            answer.setMember(member);
-
-            Article article = new Article();
-            article.setArticleId(articleId);
-            answer.setArticle(article);
-        }
+        final String content = "답변의 내용";
+        Answer answer = createDummyAnswer(articleId, answerId, content, email);
 
         final String updateContent = "변경된 답변의 내용";
-        Answer updateAnswer = new Answer();
-        {
-            updateAnswer.setAnswerId(answerId);
-            updateAnswer.setContent(updateContent);
-
-            Member member = new Member();
-            member.setEmail(email);
-            updateAnswer.setMember(member);
-
-            Article article = new Article();
-            article.setArticleId(articleId);
-            updateAnswer.setArticle(article);
-        }
+        Answer updateAnswer = createDummyAnswer(articleId, answerId, updateContent, email);
 
         given(answerRepository.save(answer)).willReturn(new Answer());
         given(answerRepository.findById(answer.getAnswerId())).willReturn(Optional.of(answer));
@@ -163,34 +113,79 @@ class AnswerServiceTest {
         final Long answerId = 1L;
         final String email = "email@gmail.com";
 
-        Answer answer = new Answer();
-        {
-            final String content = "답변의 내용";
-
-            answer.setAnswerId(answerId);
-            answer.setContent(content);
-
-            Article article = new Article();
-            article.setArticleId(articleId);
-            answer.setArticle(article);
-        }
+        final String content = "답변의 내용";
+        Answer answer = createDummyAnswer(articleId, answerId, content, null);
 
         final String updateContent = "변경된 답변의 내용";
-        Answer updateAnswer = new Answer();
-        {
-            updateAnswer.setAnswerId(answerId);
-            updateAnswer.setContent(updateContent);
+        Answer updateAnswer = createDummyAnswer(articleId, answerId, updateContent, null);
 
-            Article article = new Article();
-            article.setArticleId(articleId);
-            updateAnswer.setArticle(article);
-        }
+        given(answerRepository.save(answer)).willReturn(new Answer());
+        given(answerRepository.findById(answer.getAnswerId())).willReturn(Optional.of(answer));
+
+        //when, then
+        assertThrows(NullPointerException.class ,() -> answerService.updateAnswer(updateAnswer));
+    }
+
+    @Test
+    @DisplayName("답변글 수정 테스트 - 답변글 없음 실패")
+    @WithMockUser(username = "email@gmail.com", roles = "USER")
+    void updateAnswerTest_NotExistAnswer_Fail() {
+        //given
+        final Long articleId = 1L;
+        final Long answerId = 1L;
+        final String email = "email@gmail.com";
+
+        final String content = "답변의 내용";
+        Answer answer = createDummyAnswer(articleId, answerId, content, email);
+
+        final String updateContent = "변경된 답변의 내용";
+        Answer updateAnswer = createDummyAnswer(articleId, null, updateContent, email);
 
         given(answerRepository.save(answer)).willReturn(new Answer());
         given(answerRepository.findById(answer.getAnswerId())).willReturn(Optional.of(answer));
 
         //when, then
         assertThrows(BusinessLogicException.class ,() -> answerService.updateAnswer(updateAnswer));
+    }
+
+    @Test
+    @DisplayName("답변글 수정 테스트 - 질문글 없음 실패")
+    @WithMockUser(username = "email@gmail.com", roles = "USER")
+    void updateAnswerTest_NotExistArticle_Fail() {
+        //given
+        final Long articleId = 1L;
+        final Long answerId = 1L;
+        final String email = "email@gmail.com";
+
+        final String content = "답변의 내용";
+        Answer answer = createDummyAnswer(articleId, answerId, content, email);
+
+        final String updateContent = "변경된 답변의 내용";
+        Answer updateAnswer = createDummyAnswer(null, answerId, updateContent, email);
+
+        given(answerRepository.save(answer)).willReturn(new Answer());
+        given(answerRepository.findById(answer.getAnswerId())).willReturn(Optional.of(answer));
+
+        //when, then
+        assertThrows(BusinessLogicException.class ,() -> answerService.updateAnswer(updateAnswer));
+    }
+
+    private static Answer createDummyAnswer(Long articleId, Long answerId,
+                                            String content, String email) {
+        Answer answer = new Answer();
+
+        answer.setAnswerId(answerId);
+        answer.setContent(content);
+
+        Member member = new Member();
+        member.setEmail(email);
+        answer.setMember(member);
+
+        Article article = new Article();
+        article.setArticleId(articleId);
+        answer.setArticle(article);
+
+        return answer;
     }
 
     @Test
